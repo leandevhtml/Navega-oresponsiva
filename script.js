132dcddab8368ad1cca20862df355e37e0cce01b
leandro Gonçalves <leandrogs2003@gmail.com>
2022-12-18 18:31:04 -0300

    Navegaçãomenu

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
